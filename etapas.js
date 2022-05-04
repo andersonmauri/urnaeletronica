15ed33689bb299049029e83e9f5c989b9df5715f
Anderson Mauri <maurianderson@outlook.com>
2022-05-04 16:59:30 -0300

    Upload da aplicação

---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo : 'VEREADOR',
+        numeros : 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'FUlano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {
+                        url:'321.jpeg', legenda: 'VEREADOR', small: false}
+                ]
+            },
+        {
+                numero: '77222',
+                nome: 'Beltrano de Tal',
+                partido: 'DEFG',
+                fotos:[
+                    {
+                        url: '789.jpg', legenda: 'Vereador'}
+                ]
+        },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros:2,
+        candidatos:[
+            {
+                numero:'17',
+                nome:'Bolsonaro',
+                partido: 'SFG',
+                vice: 'Zul',
+                fotos:[
+                    {
+                        url:'17.jfif', legenda:'Prefeito'},
+                    {   url:'17-vice.jpg', legenda:'Vice-Prefeito', small: true
+                    }
+                ]
+            },
+
+            {
+                numero:'13',
+                nome:'Lula',
+                partido: 'SFG',
+                vice: 'Zul',
+                fotos:[
+                    {
+                        url:'13.jfif', legenda:'Prefeito'},
+                    {   url:'13-vice.jpg', legenda:'Vice-Prefeito', small: true
+                    }
+                ]
+            }
+        ]
+    }
+]
+
